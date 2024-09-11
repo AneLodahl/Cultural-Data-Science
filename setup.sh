4a70802e9fa95e2418290ca86d8c899f70b965f1
Ane Lodahl <ane.lodahl@gmail.com>
2024-09-11 14:59:26 +0200

    now with setup for credentials

---
+++ setup.sh
@@ -0,0 +1,3 @@
+git config --global user.email "ane.lodahl@gmail.com"
+git config --global user.name "AneLodahl" #a script that adds credentials
+#run this using "bash setup.sh" when in the correct directory
